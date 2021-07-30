5ebf13b7303138aa73639910597ec12cac3bd506
DJ Dixon <gospelman150@gmail.com>
2021-07-30 08:39:39 -0500

    Added toggle button

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -13,7 +13,6 @@ public:
     //==============================================================================
     MainComponent();
     ~MainComponent() override;
-
     //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
@@ -21,7 +20,7 @@ public:
 private:
     //==============================================================================
     // Your private member variables go here...
-
+    juce::ToggleButton toggleButton;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
